8beb2f647520034f2c1e418070face1d68933224
Alexandre Birdi <alexandre.birdi@gmail.com>
2026-04-06 19:23:39 +0200

    refactor: typo

--- anomaly_detector.py
+++ anomaly_detector.py
@@ -42,7 +42,7 @@ class AnomalyDetector():
             frobenius_difference = self.frobenius_norm(matrix1, matrix2)
             sample_list.append(frobenius_difference)
             if (i + 1) % 5 == 0:
-                print(f'Calculating Threshold. mStep {i+1}/{samples}', end='\r')
+                print(f'Calculating Threshold. Step {i+1}/{samples}', end='\r')
         return np.mean(sample_list) + 3 * np.std(sample_list)
     
             
