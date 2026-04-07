5bccbf8c0812305421eec113dc2d88d98b4264d0
Alexandre Birdi <alexandre.birdi@gmail.com>
2026-04-07 02:17:24 +0200

    added doc strings

--- lorenz_physics.py
+++ lorenz_physics.py
@@ -2,7 +2,21 @@
 # -*- coding: utf-8 -*-
 TRANSIENT_STEPS = 100
 class LorenzPhysics:
+    '''Simulates the Lorenz attractor using RK4 integration.
+
+    Generates 3D trajectories from the Lorenz system of differential
+    equations, parameterised by sigma, rho, and beta.
+    '''
     def __init__(self, sigma, rho, beta, steps=1100, x=1, y=1, z=1):
+        '''Initialises the Lorenz system.
+        :param:
+            sigma: Prandtl number controlling the rate of convective overturning.
+            rho: Rayleigh number controlling the temperature difference.
+            beta: Geometric factor of the convection cell.
+            steps: Number of integration steps.
+            x: Initial x position.
+            y: Initial y position.
+            z: Initial z position.'''
         self.sigma = sigma
         self.rho = rho
         self.beta = beta
@@ -21,6 +35,14 @@ class LorenzPhysics:
 
         
     def path(self):
+        '''Integrate the Lorenz system and return the trajectory.
+
+        Uses 4th-order Runge-Kutta. The first TRANSIENT_STEPS points
+        are discarded to remove transient behaviour.
+
+        :return
+            List of (x, y, z) tuples along the attractor.
+        '''
         for i in range(self.steps):
             
             k1 = self._lorenz_derivatives(self.x, self.y, self.z)
@@ -46,13 +68,22 @@ class LorenzPhysics:
         return self.trajectory[TRANSIENT_STEPS:]
     
     def _lorenz_derivatives(self, x, y, z):
+        '''Compute the Lorenz system derivatives at a given point.
+        :param x: The x position of the Lorenz system.
+        :param y: The y position of the Lorenz system.
+        :param z: The z position of the Lorenz system.
+
+        :return
+            tuple of derivatives (dx, dy, dz
+            '''
         dx = self.sigma * (y - x)
         dy = x *  (self.rho - z) - y
         dz = (x * y) - (self.beta * z)
         return dx, dy, dz
     
     def reset(self):
-        
+        '''reset the system state and trajectory to inital conditions.
+        '''
         self.trajectory = []
         
         self.x = self.x0
