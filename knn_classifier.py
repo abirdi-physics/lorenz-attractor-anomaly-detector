8250083a30d10a37e0067031f36ecb0a9a09b5a9
Alexandre Birdi <alexandre.birdi@gmail.com>
2026-04-07 02:08:24 +0200

    added doc strings

--- knn_classifier.py
+++ knn_classifier.py
@@ -4,13 +4,26 @@ from spd_manifold import SPDManifold
 from collections import Counter
 
 class KNNClassifier():
+    '''K-nearest neighbours classifier on the SPD manifold.
+
+    Classifies anomalous covariance matrices by finding the closest
+    entries in a pre-computed atlas using geodesic distance, then
+    returning the most common fault label among the k nearest neighbours.
+    '''
     def __init__(self):
+        '''Load covariance matrix atlas from covariance_map.json'''
         with open ('covariance_map.json', 'r') as f:
             self.covariance_map = json.load(f)
         self.spd_manifold = SPDManifold()
         self.covariance_matrices = np.array(self.covariance_map['Atlas'])
 
     def classify(self, matrix, k):
+        '''Classify an anomalous covariance matrix by fault type.
+
+        :param matrix: Covariance matrix to classify
+        :param k: The number of nearest neighbours to consider
+
+        :return String label of the most common fault, or 'No Fault Detected'''
         distance = []
         for i in range(len(self.covariance_matrices)):
             length = self.spd_manifold.distance(matrix, self.covariance_matrices[i])
